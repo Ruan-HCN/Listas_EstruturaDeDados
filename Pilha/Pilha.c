62e39875a6a6d581a55945c868f88c92f299ed8d
Ruan Henry Cipriano do Nascimento <163060190+Ruan-HCN@users.noreply.github.com>
2024-11-27 10:13:31 -0300

    Finish Pilha

--- Pilha/Pilha.c
+++ Pilha/Pilha.c
@@ -1,11 +1,11 @@
 /******************************************************************
  * Nome: Pilha                                                    *
- * Descrição: Implementação de uma pilha para manipulação de      * 
- *            produtos. Este código inclui as principais          * 
- *            operações da lista, como:                           *      
+ * DescriÃ§Ã£o: ImplementaÃ§Ã£o de uma pilha para manipulaÃ§Ã£o de      * 
+ *            produtos. Este cÃ³digo inclui as principais          * 
+ *            operaÃ§Ãµes da lista, como:                           *      
  *                                                                *
  * Autor: Ruan Henry                                              *
- * Última alteração: 26/11/2024                                   *
+ * Ãšltima alteraÃ§Ã£o: 26/11/2024                                   *
  ******************************************************************/
 
 
@@ -34,27 +34,27 @@ typedef struct {
 }Pilha;
 
 
-//Função para Limpar o Buffer
+//FunÃ§Ã£o para Limpar o Buffer
 void limparBuffer() {
     char ch;
-    while ((ch = getchar()) != '\n' && ch != EOF); // Consome o buffer até encontrar o '\n' ou EOF
+    while ((ch = getchar()) != '\n' && ch != EOF); // Consome o buffer atÃ© encontrar o '\n' ou EOF
 }
 
 
 /* Nome: criarProduto
- * Parâmetro: id - identificador único do produto
- *            cor_do_tenis - cor do tênis
- *            tamanho - tamanho do tênis
- *            material - material do tênis
- *            lote - número do lote
+ * ParÃ¢metro: id - identificador Ãºnico do produto
+ *            cor_do_tenis - cor do tÃªnis
+ *            tamanho - tamanho do tÃªnis
+ *            material - material do tÃªnis
+ *            lote - nÃºmero do lote
  *            valor - valor do produto
- *            genero - gênero do produto
+ *            genero - gÃªnero do produto
  * Retorno: retorna um ponteiro para um novo produto alocado dinamicamente
- * Descrição: Cria um novo produto com os atributos fornecidos e retorna
+ * DescriÃ§Ã£o: Cria um novo produto com os atributos fornecidos e retorna
  *            um ponteiro para ele.
  */
 Produto* criarProduto(int id, char *cor_do_tenis, float tamanho, char *material, int lote, float valor, char genero){
-	Produto *novo = (Produto*)malloc(sizeof(Produto)); //Aloca memória para um novo produto
+	Produto *novo = (Produto*)malloc(sizeof(Produto)); //Aloca memÃ³ria para um novo produto
 	novo->id = id;
 	strcpy(novo->cor_do_tenis, cor_do_tenis); //Define os atributos do produto
 	novo->tamanho = tamanho;
@@ -62,32 +62,62 @@ Produto* criarProduto(int id, char *cor_do_tenis, float tamanho, char *material,
 	novo->lote = lote;
 	novo->valor = valor;
 	novo->genero = genero;
-	novo->prox = NULL; //Inicializa o próximo elemento como NULL 
+	novo->prox = NULL; //Inicializa o prÃ³ximo elemento como NULL 
 	return novo;
 }
 
 
 /* Nome: excluirPilha
- * Parâmetro: pilha - ponteiro que possui o endereço da pilha
- * Descrição: Exclui todos os produtos da pilha, liberando a memória alocada
- *            e redefinindo o ponteiro de início para NULL.
+ * ParÃ¢metro: pilha - ponteiro que possui o endereÃ§o da pilha
+ * DescriÃ§Ã£o: Exclui todos os produtos da pilha, liberando a memÃ³ria alocada
+ *            e redefinindo o ponteiro de inÃ­cio para NULL.
  */
 void excluirPilha(Pilha *pilha){
-	Produto *temp = pilha->topo; //Ponteiro temporário para percorrer a pilha
+	Produto *temp = pilha->topo; //Ponteiro temporÃ¡rio para percorrer a pilha
 	
-	while(temp != NULL){ //Percorre a pilha liberando a memória de cada produto
+	while(temp != NULL){ //Percorre a pilha liberando a memÃ³ria de cada produto
 		Produto *prox = temp->prox;
 		free(temp);
 		temp = prox; 
 	}
-	pilha->topo = NULL; //Redefine o início da pilha para NULL (pilha vazia)
+	pilha->topo = NULL; //Redefine o inÃ­cio da pilha para NULL (pilha vazia)
+}
+
+
+/* Nome: verTopo
+ * ParÃ¢metro: pilha - ponteiro que possui o endereÃ§o da pilha
+ * DescriÃ§Ã£o: Mostra o elemento no topo da pilha;
+ */
+void verTopo(Pilha *pilha){
+	if(pilha->topo == NULL){
+		printf("\nPilha Vazia!\n");
+		return;
+	}
+	
+	
+	Produto *temp = pilha->topo;
+	printf("ID: %d\nCor do Tenis: %s\nTamanho: %.2f\nMaterial: %s\nLote: %d\nValor: %.2f\nGenero: %c\n\n",
+               temp->id, temp->cor_do_tenis, temp->tamanho, temp->material, temp->lote, temp->valor, temp->genero);
+	
+}
+
+
+/* Nome: push
+ * ParÃ¢metro: pilha - ponteiro para a pilha de produtos
+ *            novo - ponteiro para o novo produto que serÃ¡ inserido
+ * DescriÃ§Ã£o: Insere um novo produto no topo da pilha, deslocando os outros 
+ *            elementos para a prÃ³xima posiÃ§Ã£o.
+ */
+void push(Pilha *pilha, Produto *novo){
+	novo->prox = pilha->topo; //Faz o novo produto apontar para o topo atual
+    pilha->topo = novo;  //Define o novo produto como o topo da pilha
 }
 
 
 /* Nome: carregarDados
- * Parâmetro: pilha - ponteiro para a pilha de produtos
- *            nomeArquivo - nome do arquivo de onde os dados serão carregados
- * Descrição: Carrega os dados dos produtos armazenados em um arquivo
+ * ParÃ¢metro: pilha - ponteiro para a pilha de produtos
+ *            nomeArquivo - nome do arquivo de onde os dados serÃ£o carregados
+ * DescriÃ§Ã£o: Carrega os dados dos produtos armazenados em um arquivo
  *            para a lista, excluindo a lista existente antes de carregar.
  */
 void carregarDados(Pilha *pilha, char *nomeArquivo) {
@@ -105,7 +135,7 @@ void carregarDados(Pilha *pilha, char *nomeArquivo) {
     float tamanho, valor;
     
     while (fscanf(arquivo, "%d %s %f %s %d %f %c", &id, cor, &tamanho, material, &lote, &valor, &genero) != EOF) {
-        Produto *novo = CriarProduto(id, cor, tamanho, material, lote, valor, genero); //Cria um novo produto
+        Produto *novo = criarProduto(id, cor, tamanho, material, lote, valor, genero); //Cria um novo produto
         push(pilha, novo); //Insere o produto na pilha
     }
     
@@ -115,9 +145,9 @@ void carregarDados(Pilha *pilha, char *nomeArquivo) {
 
 
 /* Nome: pop
- * Parâmetro: pilha - ponteiro para a pilha de produtos
- * Descrição: Remove o produto do topo da pilha.
- *            Libera a memória alocada para o produto removido.
+ * ParÃ¢metro: pilha - ponteiro para a pilha de produtos
+ * DescriÃ§Ã£o: Remove o produto do topo da pilha.
+ *            Libera a memÃ³ria alocada para o produto removido.
  */
 void pop(Pilha *pilha){
 	if(pilha->topo == NULL){ //Verifica se a pilha esta vazia
@@ -132,32 +162,20 @@ void pop(Pilha *pilha){
 }
 
 
-/* Nome: push
- * Parâmetro: pilha - ponteiro para a pilha de produtos
- *            novo - ponteiro para o novo produto que será inserido
- * Descrição: Insere um novo produto no topo da pilha, deslocando os outros 
- *            elementos para a próxima posição.
- */
-void push(Pilha *pilha, Produto *novo){
-	novo->prox = pilha->topo; //Faz o novo produto apontar para o topo atual
-    pilha->topo = novo;  //Define o novo produto como o topo da pilha
-}
-
-
 /* Nome: iniciarPilhaa
- * Parâmetro: pilha - ponteiro para a pilha de produtos
- * Descrição: Inicializa a pilha de produtos, definindo o ponteiro de início
- *            como NULL, indicando que a pilha está vazia.
+ * ParÃ¢metro: pilha - ponteiro para a pilha de produtos
+ * DescriÃ§Ã£o: Inicializa a pilha de produtos, definindo o ponteiro de inÃ­cio
+ *            como NULL, indicando que a pilha estÃ¡ vazia.
  */
 void iniciarPilha(Pilha *pilha){
-	pilha->topo = NULL;   //Define o início da lista como NULL (vazia)
+	pilha->topo = NULL;   //Define o inÃ­cio da lista como NULL (vazia)
 }
 
 
 /* Nome: salvarDados
- * Parâmetro: pilha - ponteiro para a pilha de produtos
- *            nomeArquivo - nome do arquivo onde os dados serão salvos
- * Descrição: Salva os dados de todos os produtos presentes na pilha
+ * ParÃ¢metro: pilha - ponteiro para a pilha de produtos
+ *            nomeArquivo - nome do arquivo onde os dados serÃ£o salvos
+ * DescriÃ§Ã£o: Salva os dados de todos os produtos presentes na pilha
  *            em um arquivo de texto.
  */
 void salvarDados(Pilha *pilha, char *nomeArquivo) {
@@ -181,11 +199,11 @@ void salvarDados(Pilha *pilha, char *nomeArquivo) {
 
 
 int main() {
-	Lista lista;
-	lista.inicio = NULL; //Inicializa a lista como vazia
+	Pilha pilha;
+	pilha.topo = NULL; //Inicializa o topo da pilha como vazia
 	
-	//variaveis e definição de linguagem
-	int escolha, id, posicao, lote;
+	//variaveis e definiÃ§Ã£o de linguagem
+	int escolha, id, lote;
 	float tamanho, valor;
     char cor_do_tenis[30], material[30], genero;
     char NomeArquivo[] = "produtos.txt";
@@ -196,142 +214,70 @@ int main() {
 	
 	do{
 		printf("\n========================= Menu =========================");
-        printf("\n\n1. Inserir elemento no final\n");
-        printf("2. Inserir elemento no início\n");
-        printf("3. Inserir elemento em uma posição específica\n");
-        printf("4. Listar todos os elementos\n");
-        printf("5. Atualizar produto\n");
-        printf("6. Buscar produto por ID\n");
-        printf("7. Tamanho da lista\n");
-        printf("8. Excluir todos os elementos\n");
-        printf("9. Carregar dados de arquivo\n");
-        printf("10. Salvar dados em arquivo\n");
-        printf("11. Remover Elemento Especifico\n");
-        printf("12. Sair\n");
+        printf("\n\n1. Inserir elemento na pilha\n");
+        printf("2. Excluir Elemento do Topo\n");
+        printf("3. Ver Topo da Pilha\n");
+        printf("4. Excluir Pilha\n");
+        printf("5. Salvar Dados\n");
+        printf("6. Carregar Dados\n");
+        printf("7. Sair\n");
         
-		printf("\nEscolha uma opção: ");
+		printf("\nEscolha uma opÃ§Ã£o: ");
         scanf("%d", &escolha);
         
         switch(escolha){
 			 case 1:
                 printf("\n\nID: ");
                 scanf("%d", &id);
-                printf("Cor do Tênis: ");
+                printf("Cor do TÃªnis: ");
                 scanf("%s", cor_do_tenis);
                 printf("Tamanho: ");
                 scanf("%f", &tamanho);
-                LimparBuffer();
+                limparBuffer();
                 printf("Material: ");
                 scanf("%s", material);
                 printf("Lote: ");
                 scanf("%d", &lote);
                 printf("Valor: ");
                 scanf("%f", &valor);
-                LimparBuffer();
-                printf("Gênero: ");
+                limparBuffer();
+                printf("GÃªnero: ");
                 scanf(" %c", &genero);
-                InserirElemento(&lista, CriarProduto(id, cor_do_tenis, tamanho, material, lote, valor, genero)); //insere elemnto no final
+                push(&pilha, criarProduto(id, cor_do_tenis, tamanho, material, lote, valor, genero)); //insere elemnto na pilha
                 break;
             
             case 2:
-                printf("\n\nID: ");
-                scanf("%d", &id);
-                printf("Cor do Tênis: ");
-                scanf("%s", cor_do_tenis);
-                printf("Tamanho: ");
-                scanf("%f", &tamanho);
-                LimparBuffer();
-                printf("Material: ");
-                scanf("%s", material);
-                printf("Lote: ");
-                scanf("%d", &lote);
-                printf("Valor: ");
-                scanf("%f", &valor);
-                LimparBuffer();
-                printf("Gênero: ");
-                scanf(" %c", &genero);
-                InserirElementoInicio(&lista, CriarProduto(id, cor_do_tenis, tamanho, material, lote, valor, genero)); //insere elemnto no inicio
+                pop(&pilha);
                 break;
             
             case 3:
-                printf("\nPosição: ");
-                scanf("%d", &posicao);
-                
-                printf("\n\nID: ");
-                scanf("%d", &id);
-                printf("Cor do Tênis: ");
-                scanf("%s", cor_do_tenis);
-                printf("Tamanho: ");
-                scanf("%f", &tamanho);
-                LimparBuffer();
-                printf("Material: ");
-                scanf("%s", material);
-                printf("Lote: ");
-                scanf("%d", &lote);
-                printf("Valor: ");
-                scanf("%f", &valor);
-                LimparBuffer();
-                printf("Gênero: ");
-                scanf(" %c", &genero);
-                InserirElementoPosicao(&lista, CriarProduto(id, cor_do_tenis, tamanho, material, lote, valor, genero), posicao); //insere elemento na posição
+            	printf("\n");
+                verTopo(&pilha);
+                printf("\n");
                 break;
             
             case 4:
-            	printf("\n");
-                ListarElementos(&lista); //Lista todos os elementos
+            	excluirPilha(&pilha);
                 break;
             
             case 5:
-                printf("\nID do produto a ser atualizado: ");
-                scanf("%d", &id);
-                AtualizarProduto(&lista, id); //atualiza o elemento
+            	salvarDados(&pilha, NomeArquivo); //salva os dados da lista num txt
                 break;
             
             case 6:
-                printf("\nID do produto a ser buscado: ");
-                scanf("%d", &id);
-                Produto *produto = BuscarElemento(&lista, id); //busca o produto
-                if (produto != NULL) {
-                    printf("\nID: %d\nCor do Tenis: %s\nTamanho: %.2f\nMaterial: %s\nLote: %d\nValor: %.2f\nGenero: %c\n",
-                           produto->id, produto->cor_do_tenis, produto->tamanho, produto->material, produto->lote, produto->valor, produto->genero);
-                } else {
-                    printf("\nProduto não encontrado!\n");
-                }
+                carregarDados(&pilha, NomeArquivo); //carregar os dados do txt
                 break;
             
             case 7:
-                printf("\nTamanho da lista: %d\n", TamanhoLista(&lista)); //verifica quantos elementos tem a lista
-                break;
-            
-            case 8:
-                ExcluirLista(&lista); //exclui a lista
-                printf("\nLista excluída!\n");
-                break;
-            
-            case 9:
-                CarregarDados(&lista, NomeArquivo); //carregar os dados do txt
-                break;
-            
-            case 10:
-                SalvarDados(&lista, NomeArquivo); //salva os dados da lista num txt
-                break;
-            
-            case 11:
-            	printf("\nID do produto a ser removido: ");
-    			scanf("%d", &id);
-            	RemoverElemento(&lista, id);
-            	break;
-            
-            case 12:
-                ExcluirLista(&lista); //exclui todos os elementos e sai do script
+                excluirPilha(&pilha); //exclui todos os elementos e sai do script
                 printf("\nSaindo...\n");
                 break;
             
             default:
-                printf("Opção inválida!\n");
+                printf("OpÃ§Ã£o invÃ¡lida!\n");
                 continue;
 		}
-	}while(escolha != 12);
+	}while(escolha != 7);
 	
 	return 0;
 }
